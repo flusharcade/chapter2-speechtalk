ce3d6f95e019ddb49e95dec7e8d71e7ce2fe35be
Michael Williams <info@flusharcade.com>
2016-03-08 15:36:33 +1100

    First commit

---
+++ App.cs
@@ -0,0 +1,32 @@
+﻿namespace SpeechTalk
+{
+	using System;
+
+	using Xamarin.Forms;
+
+	using SpeechTalk.Pages;
+
+	public class App : Application
+	{
+		public App ()
+		{
+			MainPage = new MainPage ();
+		}
+
+		protected override void OnStart ()
+		{
+			// Handle when your app starts
+		}
+
+		protected override void OnSleep ()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume ()
+		{
+			// Handle when your app resumes
+		}
+	}
+}
+
---
+++ Pages/MainPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿namespace SpeechTalk.Pages
+{
+	using System;
+	using System.Collections.Generic;
+
+	using Xamarin.Forms;
+
+	public partial class MainPage : ContentPage
+	{
+		public MainPage ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
+
